18512824f54c3d9f4b538be002e9c81f159b15ca
smcniel <mcnes430@newschool.edu>
2017-01-25 11:45:34 -0500

    mirror layout

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+
+   var newsdata = "Trump Revives Keystone Pipeline Rejected by Obama";
+   var weatherdata = "Temperature: 40 F";
+   var time = "June 7 2016 12:40:05";
+
+   $("#news").append("<li>" + newsdata + "<li>");
+   $("#news").append("<li>" + newsdata + "<li>");
+   $("#news").append("<li>" + newsdata + "<li>");
+   $("#news").append("<li>" + newsdata + "<li>");
+   $("#news").append("<li>" + newsdata + "<li>");
+
+   $("#weather").append("<li>" + weatherdata + "<li>");
+   $("#weather").append("<li>" + weatherdata + "<li>");
+   $("#weather").append("<li>" + weatherdata + "<li>");
+   $("#weather").append("<li>" + weatherdata + "<li>");
+
+   $("#time").append("<li>" + time + "<li>");
+
+
+
+});
